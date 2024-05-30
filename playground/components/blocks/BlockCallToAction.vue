2798d26ae130511ae0306f33d8fcb9eca57dfcae
Marco <marco@freibergersyntax.dev>
2024-05-30 11:57:13 +0200

    chore(release): v1.0.1

---
+++ playground/components/blocks/BlockCallToAction.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  block: any // define your block type here
+}>()
+</script>
+
+<template>
+  <div>
+    <h1>{{ block.title }}</h1>
+    <RenderContent :content="block.subtext" />
+    <details>
+      <summary>Block data</summary>
+      <pre>{{ block }}</pre>
+    </details>
+  </div>
+</template>
